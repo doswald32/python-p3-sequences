1b1beb4f65d3abea7791eb7b0cc9eb5ccbe4009b
doswald32 <dan.oswald32@gmail.com>
2024-05-16 22:43:35 -0600

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        new_list = [0, 1]
+        i = 0
+        while i <=7:
+            new_item = new_list[i] + new_list[i + 1]
+            new_list.append(new_item)
+            i += 1
+        print (new_list)
